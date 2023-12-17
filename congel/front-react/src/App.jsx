80e4b3ce7572da839d54bc9b842a58bf4a212585
Robin <robinlethiec1@gmail.com>
2023-12-17 13:21:35 +0100

    save

---
+++ congel/front-react/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+const Testt = () => {
+  return (
+    <div>
+      <p>Hello worldddddd</p>
+    </div>
+  )
+}
+
+const App = () => {
+  return (
+    <div><h1>hello world</h1>
+      <Testt /></div>
+      
+  )
+}
+
+export default App
