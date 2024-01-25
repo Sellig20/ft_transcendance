94494ef68bc73cfb0b6638496fd0c37bab09de18
anedonia <deuxvys@gmail.com>
2024-01-25 18:18:24 +0100

    lol

---
+++ back-end/src/auth/auth.module.ts
@@ -0,0 +1,39 @@
+import { Module } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthController } from './auth.controller';
+import { PassportModule } from '@nestjs/passport';
+import { FTGuard } from './guard/FTGuard';
+import { FTstrategy } from './utils/FTstrategy';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './utils/constant';
+import { JWTstrategy } from './utils/JWTstrategy';
+import { APP_GUARD } from '@nestjs/core';
+import { UserModule } from 'src/user/user.module';
+import { Jwt2faAuthGuard } from './guard/JWTTFAauth.guard'
+import { Jwt2faStrategy } from './utils/JWT2FA.strategy';
+import { JWTAUthGuard } from './guard/JWTGuard';
+
+@Module({
+  imports: [
+    PassportModule.register({ session: false }),
+    JwtModule.register({
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+	UserModule
+  ],
+  controllers: [AuthController],
+  providers: [
+    AuthService,
+    FTGuard,
+	JWTAUthGuard,
+    FTstrategy,
+    JWTstrategy,
+	Jwt2faStrategy,
+    {
+      provide: APP_GUARD,
+      useClass: Jwt2faAuthGuard,
+    },
+  ],
+})
+export class AuthModule {}
