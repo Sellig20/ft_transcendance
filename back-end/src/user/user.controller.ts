17fd9846d0c9594788a39237ea9aa751e07be815
ldevy <deuxvys@gmail.com>
2024-02-26 01:57:47 +0100

    MERGE:FIX

--- back-end/src/user/user.controller.ts
+++ back-end/src/user/user.controller.ts
@@ -7,7 +7,6 @@ import { UsersService } from './user.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { Response } from 'express';
 import * as path from 'path';
-import * as path from 'path';
 import { IdDto, NameDto } from './dto/user.dto';
 
 @Controller('user')
