ec91972c88c3116720edbaa5bdf41d5857b9bd68
Robin <robinlethiec1@gmail.com>
2024-03-02 01:19:51 +0100

    UPDATE: DTO chat OK

--- back-end/src/chat/dto/chat.dto.ts
+++ back-end/src/chat/dto/chat.dto.ts
@@ -22,6 +22,85 @@ export class CreateChannelDto {
   idUser: number;
 }
 
+export class LeaveChannelById {
+  @IsNumber()
+  @IsNotEmpty()
+  channelid: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  userid: number;
+}
+
+export class BanChannelById {
+  @IsNumber()
+  @IsNotEmpty()
+  channelid: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  userid: number;
+}
+
+export class BlockUserById {
+  @IsNumber()
+  @IsNotEmpty()
+  userid: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  userToBlock: number;
+}
+
+export class SetAdminById {
+  @IsNumber()
+  @IsNotEmpty()
+  channelId: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  userToSet: number;
+}
+
+export class MuteById {
+  @IsNumber()
+  @IsNotEmpty()
+  channelId: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  userId: number;
+}
+
+export class InviteUser {
+  @IsNumber()
+  @IsNotEmpty()
+  channelid: number;
+
+  @IsString()
+  @IsNotEmpty()
+  userid: string;
+}
+
+export class InviteUserId {
+  @IsNumber()
+  @IsNotEmpty()
+  channelid: number;
+
+  @IsNumber()
+  @IsNotEmpty()
+  userid: number;
+}
+
+export class ChangePassword {
+  @IsString()
+  password: string;
+
+  @IsNumber()
+  @IsNotEmpty()
+  channelid: number;
+}
+
 // export class UserDetails {
 //   @IsEmail()
 //   @IsNotEmpty()
